32eee93c7feed3dd50e09b9c4c1d724fc2ab5056
franciscoAC <apazafrancisco20@gmail.com>
2025-08-21 10:44:54 -0500

    sdsd

---
+++ Asistencia/Asistencia/src/main/java/org/example/asistencia/aplication.java
@@ -0,0 +1,9 @@
+package org.example.asistencia;
+
+public class aplication {
+    public static void main(String[] args) {
+System.out.println("iniciando asistencia DMP");
+AsistenciaApplication.main(args);
+
+    }
+}
