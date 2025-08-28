2b9dff3493ba92214cff347b679e28f4361a8db7
franciscoAC <apazafrancisco20@gmail.com>
2025-08-28 12:10:03 -0500

    sdsd

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/EstudianteRepository.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/EstudianteRepository.java
@@ -1,6 +1,5 @@
 package pe.edu.upeu.asistencia.repositorio;
 
-import javafx.beans.property.BooleanPropertyBase;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import pe.edu.upeu.asistencia.modelo.Estudiante;
@@ -9,15 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EstudianteRepository {
-    List<Estudiante> estudiantes=new ArrayList<>();
+    public List<Estudiante> estudiantes=new ArrayList<>();
 
-    List <Estudiante> findallEstudiantes () {
-            estudiantes.add(new Estudiante(
-                    new SimpleStringProperty("juan"),
-                    new SimpleBooleanProperty(true)
-                    )
-            );
-            return estudiantes;
+    public   List<Estudiante> findAllestudiantes(){
+        estudiantes.add(new Estudiante(
+                new SimpleStringProperty("Juan"),
+                new SimpleBooleanProperty(true)
+
+        ));
+        return  estudiantes;
     }
-}
 
+
+}
